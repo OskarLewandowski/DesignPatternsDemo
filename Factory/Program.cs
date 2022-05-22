58ebeef2d721d483e8190855ba28d382c7e94d16
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-22 15:17:02 +0200

    add files factory

---
+++ Factory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapeFactory = new ShapeFactory();
+
+            var circle = shapeFactory.CreateShape(ShapeType.Circle);
+            circle.Render();
+
+            var triangle = shapeFactory.CreateShape(ShapeType.Triangle);
+            triangle.Render();
+
+            var rectangel = shapeFactory.CreateShape(ShapeType.Rectangle);
+            rectangel.Render();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Factory/ShapeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class ShapeFactory
+    {
+        public Shape CreateShape(ShapeType shapeType)
+        {
+            //we can use if or switch
+            switch (shapeType)
+            {
+                case ShapeType.Circle:
+                    return new Circle();
+                case ShapeType.Rectangle:
+                    return new Rectangle();
+                case ShapeType.Triangle:
+                    return new Triangle();
+                default:
+                    throw new Exception($"Shape type {shapeType} is not handled");
+            }
+        }
+    }
+}
