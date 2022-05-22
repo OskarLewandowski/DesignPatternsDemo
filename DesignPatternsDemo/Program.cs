67a277a2fed14f7fdd810415ab7cca07dfd4d54f
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-22 11:33:02 +0200

    Add project files.

---
+++ DesignPatternsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatternsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> designPatternsItems = new List<string>()
+            {
+                "Builder"
+            };
+
+            Console.WriteLine("Design Patterns");
+            for (int i = 0; i < designPatternsItems.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}) {designPatternsItems[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
