dbd9cea538e035cae08d67427ad01ab4510a61d4
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-22 17:59:30 +0200

    add files singleton

---
+++ Singleton/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Configuration
+    {
+        private static Configuration _instance = null;
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+
+        private Configuration()
+        {
+
+        }
+
+        public static Configuration GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Configuration();
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cfg = new Configuration();
+            var cfg = Configuration.GetInstance();
+            var cfg2 = Configuration.GetInstance();
+
+            if (ReferenceEquals(cfg, cfg2))
+            {
+                Console.WriteLine("Configuration is a singleton");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
