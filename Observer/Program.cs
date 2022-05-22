8c77b2d806ede22e4d367dc29664c0f62197067e
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-22 22:23:59 +0200

    add files observer

---
+++ Observer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriber1 = new Subscriber("Oskar");
+            var subscriber2 = new Subscriber("Kasia");
+
+            var publisher = new Publisher();
+
+            publisher.Subscribe(subscriber1);
+            publisher.Subscribe(subscriber2);
+
+            publisher.Notify("test message ąćżź");
+
+            publisher.UnSubscribe(subscriber1);
+
+            publisher.Notify("second test message ąćżź");
+
+
+            Console.ReadLine();
+        }
+    }
+}
