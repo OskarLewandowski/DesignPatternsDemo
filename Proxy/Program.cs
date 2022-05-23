2ade1eeede147317c1d9c933d1b3ff8448985c13
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-23 21:02:18 +0200

    add files proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //with not caching 
+            Console.WriteLine("No caching");
+            YouTubeService youTubeService = new YouTubeService();
+            youTubeService.GetVideo(10);
+            youTubeService.GetVideo(11);
+            youTubeService.GetVideo(10);
+            youTubeService.GetVideo(10);
+            youTubeService.GetVideo(11);
+
+            Console.WriteLine("\nCaching");
+            //with caching 
+            ProxyYouTubeService proxyYouTubeService = new ProxyYouTubeService(youTubeService);
+            proxyYouTubeService.GetVideo(10);
+            proxyYouTubeService.GetVideo(11);
+            proxyYouTubeService.GetVideo(10);
+            proxyYouTubeService.GetVideo(10);
+            proxyYouTubeService.GetVideo(11);
+
+            Console.ReadLine();
+        }
+    }
+}
