339c8585b41f474a33248c5ace319ca752601371
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-23 15:49:05 +0200

    add files state

---
+++ State/Context.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class Context
+    {
+        private State _currentState;
+
+        public int AvailableCash { get; set; } = 10000;
+
+        public Context()
+        {
+            _currentState = new NoCard(this);
+        }
+
+        public void ChangeState(State newState)
+        {
+            _currentState = newState;
+        }
+
+        public void EjectCard()
+        {
+            _currentState.EjectCard();
+        }
+
+        public void InsertCard()
+        {
+            _currentState.InsertCard();
+        }
+
+        public void InsertPin(int pin)
+        {
+            _currentState.InsertPin(pin);
+        }
+
+        public void WithdrawCash(int amount)
+        {
+            _currentState.WithdrawCash(amount);
+        }
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+
+            Console.WriteLine(1);
+            context.EjectCard();
+            context.InsertPin(9999);
+            context.InsertCard();
+            context.WithdrawCash(222);
+
+            Console.WriteLine(2);
+            context.InsertCard();
+            context.InsertPin(1111);
+            context.WithdrawCash(5000);
+
+            Console.WriteLine(3);
+            context.InsertCard();
+            context.InsertPin(8888);
+            context.WithdrawCash(5000);
+
+            Console.WriteLine(4);
+            context.EjectCard();
+            context.InsertCard();
+            context.InsertPin(1111);
+            context.InsertCard();
+            context.InsertPin(8888);
+            context.WithdrawCash(5000);
+            context.InsertCard();
+
+            Console.ReadLine();
+        }
+    }
+}
