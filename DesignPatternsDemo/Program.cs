ba72ce9777c5f9f81ddd6d32af73c4e7fd26fac4
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-23 21:23:12 +0200

    change main program

--- DesignPatternsDemo/Program.cs
+++ DesignPatternsDemo/Program.cs
@@ -5,18 +5,52 @@ namespace DesignPatternsDemo
 {
     internal class Program
     {
+        public static void Display(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine($"\n{list[i].ToUpper()}:");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}) {list[i]}");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            List<string> designPatternsItems = new List<string>()
-            {
-                "Builder"
-            };
+            List<List<string>> designPatternsList = new List<List<string>>();
 
-            Console.WriteLine("Design Patterns");
-            for (int i = 0; i < designPatternsItems.Count; i++)
+            List<string> creationalDesignPatternsItems = new List<string>()
             {
-                Console.WriteLine($"{i + 1}) {designPatternsItems[i]}");
+                "creational", "Builder", "Factory", "Abstract factory", "Prototype", "Singleton",
+            };
+            designPatternsList.Add(creationalDesignPatternsItems);
+
+            List<string> behavioralDesignPatternsItems = new List<string>()
+            {
+                "behavioral", "Strategy", "Observer", "Mediator", "Chain of responsibility",
+                "Template method", "State"
+            };
+            designPatternsList.Add(behavioralDesignPatternsItems);
+
+            List<string> structuralDesignPatternsItems = new List<string>()
+            {
+                "structural", "Adapter", "Facade", "Decorator", "Proxy"
+            };
+            designPatternsList.Add(structuralDesignPatternsItems);
+
+            Console.WriteLine("Design Patterns:");
+
+            for (int i = 0; i < designPatternsList.Count; i++)
+            {
+                Display(designPatternsList[i]);
             }
+
+
             Console.ReadLine();
         }
     }
