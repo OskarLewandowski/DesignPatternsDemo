9476daab0dde0b33db6d8b2b30928842558f91d0
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-23 20:31:03 +0200

    add files decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizzaBase = new MediumPizza();
+
+            var mediumPizzaWithCheese = new CheesePizzaDecorator(pizzaBase);
+            Console.WriteLine($"mediumPizzaWithCheese: {mediumPizzaWithCheese.CalculatePrice()} pln");
+
+            var mediumPizzaWithCheeseAndSalami = new SalamiPizzaDecorator(mediumPizzaWithCheese);
+            Console.WriteLine($"mediumPizzaWithCheeseAndSalami: {mediumPizzaWithCheeseAndSalami.CalculatePrice()} pln");
+
+            var pizzaNumberTwo = new HamPizzaDecorator(mediumPizzaWithCheeseAndSalami);
+            Console.WriteLine($"pizzaNumberTwo: {pizzaNumberTwo.CalculatePrice()} pln");
+
+            var pizzaNumberTwoX2Salami = new SalamiPizzaDecorator(pizzaNumberTwo);
+            Console.WriteLine($"pizzaNumberTwoX2Salami: {pizzaNumberTwoX2Salami.CalculatePrice()} pln");
+
+            Console.ReadLine();
+        }
+    }
+}
